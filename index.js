ee0ce60f16706277176d00d355ab12ef74b667a4
fmenis <tsac041@tecnicosuperiorekennedy.it>
2016-08-17 10:26:18 +0200

    flexgrid - fix

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ function compile(str, path) {
 							CONTENT RESPONSE
 *****************************************************************************/
 
-app.use(compress({threshold: 0}));
 app.use(express.static(__dirname+'/public'))
 
 app.use(logger('tiny'))
