8757df5be54b6249accd313fe2c65da37abbab9e
fmenis <tsac041@tecnicosuperiorekennedy.it>
2016-08-22 09:28:45 +0200

    reorder files

--- public/js/app.js
+++ public/js/app.js
@@ -5,36 +5,31 @@ var molo = angular.module('MoloWebsite', []);
 
 molo.run(function(coordinatesService) {
 
-
 	function loadView() {
 
 		var hour = new Date().getHours();
 
 		if(hour >= 6 && hour <= 11) {
 			coordinatesService.setMoment('morning')
-			console.log('morning view loaded');
-			return
+			return console.log('morning view loaded');
 		}
 
 		if(hour >= 12 && hour <= 18) {
 			//$scope.moment = 'afternoon'
 			coordinatesService.setMoment('afternoon')
-			console.log('afternoon view loaded');
-			return
+			return console.log('afternoon view loaded');
 		}
 
 		if(hour >= 19 && hour <= 21) {
 			//$scope.moment = 'evening';
 			coordinatesService.setMoment('evening')
-			console.log('evening view loaded');
-			return
+			return console.log('evening view loaded');
 		}
 
 		//For night loading
 		//$scope.moment = 'night';
 		coordinatesService.setMoment('night')
 		console.log('night view loaded');
-
 	}
 	loadView();
 
