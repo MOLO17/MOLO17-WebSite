8c208bf66bb848cb9d89d80afe3d8781629e4b32
fmenis <tsac041@tecnicosuperiorekennedy.it>
2016-08-27 18:52:11 +0200

    ingrandite barche in day e afternoon view - colore aziendale links

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -197,7 +197,7 @@ molo.controller('mainController', function($scope, $http, $timeout, $interval, $
 	function whichBoat() {
 
 		//var moment = coordinatesService.getMoment();
-		var moment = 'day';
+		var moment = 'evening';
 		var boat;
 
 		if(moment === 'morning')
