8e33577119384af8742c5baab0d09b21d4b3cac7
fmenis <tsac041@tecnicosuperiorekennedy.it>
2016-09-02 13:17:51 +0200

    improve logic

--- public/js/controllers/footer.controller.js
+++ public/js/controllers/footer.controller.js
@@ -19,12 +19,8 @@ molo.controller('footerController', function($scope) {
 		'Produttore di apparati per il networking, il wireless, la security e i sistemi di accesso ad internet in grado di migliorare la connettività delle applicazioni multimediali.',
 		'Con sede a Seattle, WatchGuard ha implementato in tutto il mondo quasi un milione di appliance integrate e multifunzione per la gestione delle minacce, sia nelle piccole e medie imprese, sia nelle grandi aziende distribuite.'
 	]
-	//amazonText = 'Piattaforma di servizi cloud in grado di offrire con sicurezza e scalabilità storage di database, distribuzione di contenuti e funzionalità.'; 
-	//herokuText = 'Piattaforma di programmazione che permette alle aziende di creare, rilasciare, monitorare e scalare applicazioni, Heroku è il modo più veloce per passare dall’idea al progetto.';
-	//zyxelText = 'Produttore di apparati per il networking, il wireless, la security e i sistemi di accesso ad internet in grado di migliorare la connettività delle applicazioni multimediali.';
-	//watchguardText = 'Con sede a Seattle, WatchGuard ha implementato in tutto il mondo quasi un milione di appliance integrate e multifunzione per la gestione delle minacce, sia nelle piccole e medie imprese, sia nelle grandi aziende distribuite.';
 
-	vm.partnerText = sponsorList[0]
+	vm.sponsorText = sponsorList[0]
 
 	$scope.currentSponsorIndex = 0;
 
@@ -34,25 +30,25 @@ molo.controller('footerController', function($scope) {
 
 		if(sponsor === 'amazon') {
 			$scope.currentSponsorIndex = 0;
-			return vm.partnerText = sponsorList[0];
+			return vm.sponsorText = sponsorList[0];
 		}
 			
 
 		if(sponsor === 'heroku') {
 			$scope.currentSponsorIndex = 1;
-			return vm.partnerText = sponsorList[1];
+			return vm.sponsorText = sponsorList[1];
 		}
 			
 
 		if(sponsor === 'zyxel') {
 			$scope.currentSponsorIndex = 2;
-			return vm.partnerText = sponsorList[2];
+			return vm.sponsorText = sponsorList[2];
 		}
 			
 
 		if(sponsor === 'watchguard') {
 			$scope.currentSponsorIndex = 3;
-			return vm.partnerText = sponsorList[3];
+			return vm.sponsorText = sponsorList[3];
 		}
 					
 	}
