a7cd55a95c6b0908bb36907b8da0bc0cb787b882
fmenis <tsac041@tecnicosuperiorekennedy.it>
2016-09-02 12:15:52 +0200

    work on pages

--- public/js/controllers/footer.controller.js
+++ public/js/controllers/footer.controller.js
@@ -13,27 +13,48 @@ molo.controller('footerController', function($scope) {
 	vm.string3 = 'Infrastrutture Cloud';
 	vm.github = 'Contribuisci al progetto su';
 
-	vm.partnerText;
-	amazonText = 'Piattaforma di servizi cloud in grado di offrire con sicurezza e scalabilità storage di database, distribuzione di contenuti e funzionalità.'; 
-	herokuText = 'Piattaforma di programmazione che permette alle aziende di creare, rilasciare, monitorare e scalare applicazioni, Heroku è il modo più veloce per passare dall’idea al progetto.';
-	zyxelText = 'Produttore di apparati per il networking, il wireless, la security e i sistemi di accesso ad internet in grado di migliorare la connettività delle applicazioni multimediali.';
-	watchguardText = 'Con sede a Seattle, WatchGuard ha implementato in tutto il mondo quasi un milione di appliance integrate e multifunzione per la gestione delle minacce, sia nelle piccole e medie imprese, sia nelle grandi aziende distribuite.';
+	var sponsorList = [
+		'Piattaforma di servizi cloud in grado di offrire con sicurezza e scalabilità storage di database, distribuzione di contenuti e funzionalità.',
+		'Piattaforma di programmazione che permette alle aziende di creare, rilasciare, monitorare e scalare applicazioni, Heroku è il modo più veloce per passare dall’idea al progetto.',
+		'Produttore di apparati per il networking, il wireless, la security e i sistemi di accesso ad internet in grado di migliorare la connettività delle applicazioni multimediali.',
+		'Con sede a Seattle, WatchGuard ha implementato in tutto il mondo quasi un milione di appliance integrate e multifunzione per la gestione delle minacce, sia nelle piccole e medie imprese, sia nelle grandi aziende distribuite.'
+	]
+	//amazonText = 'Piattaforma di servizi cloud in grado di offrire con sicurezza e scalabilità storage di database, distribuzione di contenuti e funzionalità.'; 
+	//herokuText = 'Piattaforma di programmazione che permette alle aziende di creare, rilasciare, monitorare e scalare applicazioni, Heroku è il modo più veloce per passare dall’idea al progetto.';
+	//zyxelText = 'Produttore di apparati per il networking, il wireless, la security e i sistemi di accesso ad internet in grado di migliorare la connettività delle applicazioni multimediali.';
+	//watchguardText = 'Con sede a Seattle, WatchGuard ha implementato in tutto il mondo quasi un milione di appliance integrate e multifunzione per la gestione delle minacce, sia nelle piccole e medie imprese, sia nelle grandi aziende distribuite.';
+
+	vm.partnerText = sponsorList[0]
+
+	$scope.currentSponsorIndex = 0;
 
 
-	//On mouseover, this function is triggered and changes dinamicalli the text below the images
+	//On mouseover, this function is triggered and changes dinamically the text below the images
 	$scope.changeText = function(sponsor) {
 
-		if(sponsor === 'amazon')
-			return vm.partnerText = amazonText;
+		if(sponsor === 'amazon') {
+			$scope.currentSponsorIndex = 0;
+			return vm.partnerText = sponsorList[0];
+		}
+			
 
-		if(sponsor === 'heroku')
-			return vm.partnerText = herokuText;
+		if(sponsor === 'heroku') {
+			$scope.currentSponsorIndex = 1;
+			return vm.partnerText = sponsorList[1];
+		}
+			
 
-		if(sponsor === 'zyxel')
-			return vm.partnerText = zyxelText;
+		if(sponsor === 'zyxel') {
+			$scope.currentSponsorIndex = 2;
+			return vm.partnerText = sponsorList[2];
+		}
+			
 
-		if(sponsor === 'watchguard')
-			return vm.partnerText = watchguardText;		
+		if(sponsor === 'watchguard') {
+			$scope.currentSponsorIndex = 3;
+			return vm.partnerText = sponsorList[3];
+		}
+					
 	}
 
 
